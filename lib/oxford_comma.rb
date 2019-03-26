9882b10eca782080112b49f145c04dc775b4a1ad
minh nguyen <imnotminh@gmail.com>
2019-03-26 05:28:20 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length==1
+    return array.shift
+    elsif array.length==2
+    return "#{array[0]} and #{array[1]}"
+    else
+    x=array.pop
+  
+  return array.join(", ")<<(", and #{x}")
+end
 end
